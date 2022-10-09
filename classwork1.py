fef339eba5263367f7851d3c4de6f7b091481ea6
Yash Kumar Gupta <ykgupta2411@gmail.com>
2022-10-09 11:41:45 +0530

    Added Calculator Simple

---
+++ classwork1.py
@@ -0,0 +1,4 @@
+a = int(input("Enter a number: "))
+b = int(input("Enter another number: "))
+
+print(f"The following operations on {a} and {b} are:-\n{a}+{b}={a+b}\n{a}-{b}={a-b}\n{a}*{b}={a*b}\n{a}/{b}={a/b}\n{a}**{b}={a**b}\n{a}//{b}={a//b}\n{a}%{b}={a%b}")
