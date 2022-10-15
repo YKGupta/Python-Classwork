0009c9e7d0553a7a7753969d28e930f843495ba5
Yash Kumar Gupta <ykgupta2411@gmail.com>
2022-10-15 19:45:51 +0530

    Added lists example

--- classwork2.py
+++ classwork2.py
@@ -2,3 +2,30 @@
 l = ["Tea", "Coffee", "Chips", "Softdrink", "Chocolate", "Burger", "Chawal", "Dal"]
 print(type(l))
 print(l[0], l[1], l[2], l[3], l[4], l[5], l[6], l[7])
+
+print(l)
+
+l.append("Paneer")
+l.append("Biscuits")
+
+print(l)
+
+l.pop()
+
+print(l)
+
+l.pop(1)
+
+print(l)
+
+l.insert(3, "Roti")
+
+print(l)
+
+del l[0]
+
+print(l)
+
+l.clear()
+
+print(l)
