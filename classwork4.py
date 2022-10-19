454e953093c1186c37f7b0dcbf06d445fce06bf4
Yash Kumar Gupta <ykgupta2411@gmail.com>
2022-10-19 09:40:48 +0530

    Simple questions via loop

---
+++ classwork4.py
@@ -0,0 +1,4 @@
+# Printing all elements multiplied by 2
+l = [11, 12, 13, 14, 15]
+for i in l:
+    print(i*2, end=" ")
---
+++ classwork5.py
@@ -0,0 +1,3 @@
+# Swap first and last characters of a string
+s = input()
+print(s[-1] + s[1:-1] + s[0])
