005969dc821e39cd15c365bdc838e0b8b5e6cacf
Yash Kumar Gupta <ykgupta2411@gmail.com>
2022-10-26 09:47:58 +0530

    Revision of slicing of list

--- classwork9.py
+++ classwork9.py
@@ -3,3 +3,7 @@ n = int(input("Enter range(1-10): "))
 l = [1 , 2, 3, 4, 5, 6, 7, 8, 9, 10]
 for i in l[:n]:
     print(i, "cubed =", i**3)
+
+print("\nElements from", str(n+1) + "th", "position till end:")
+for i in l[n:]:
+    print(i, "cubed =", i**3)
