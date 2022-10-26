1b887cf983cc7c57f493754d4d6ba6301e374026
Yash Kumar Gupta <ykgupta2411@gmail.com>
2022-10-26 09:30:38 +0530

    For loops revision

---
+++ classwork8.py
@@ -0,0 +1,12 @@
+# For loop revision
+# Printing age after 2 years
+print("Age after two years example:")
+age = [11, 13, 29, 31, 37, 43]
+for i in age:
+    print(i, "is now", i+2, "years old")
+
+# Printing square of elementsc
+print("\nSquraing elements of list:")
+l = [1, 2, 3, 4, 5]
+for i in l:
+    print(i, " squared =", i**2)
