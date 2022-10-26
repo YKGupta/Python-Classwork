197b64b4e1416010fc4f4a9e34aae9a1d13568ae
Yash Kumar Gupta <ykgupta2411@gmail.com>
2022-10-26 08:42:15 +0530

    Added a practice file

---
+++ classwork6.py
@@ -0,0 +1,7 @@
+# Practicing output based problem
+
+a = [0, 1, 2, 3]
+for a[-1] in a:
+    print(a)
+    # the list gets updated each time since a[-1] actually changes a's a[-1] to
+    # 0 then 1 then 2 then 2
