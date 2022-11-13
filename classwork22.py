1cb69a6bea498ca561ae508dc87164f7e00e9433
Yash Kumar Gupta <ykgupta2411@gmail.com>
2022-11-13 12:38:36 +0530

    Added basic use of functions

---
+++ classwork22.py
@@ -0,0 +1,25 @@
+# Generate output
+# Hello, good afternoon!
+# Hi
+# Hello, good afternoon!
+# Hello, good afternoon!
+# Hey
+# How are you doing
+# Hello, good afternoon!
+# Hello, good afternoon!
+# What are you upto?
+# Hello, good afternoon!
+
+# Use of functions
+def greetHello():
+    print("Hello, good afternoon!")
+greetHello()
+print("Hi")
+greetHello()
+greetHello()
+print("Hey")
+print("How are you doing")
+greetHello()
+greetHello()
+print("What are you upto?")
+greetHello()
