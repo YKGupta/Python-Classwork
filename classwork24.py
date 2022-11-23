5070fc6ab1eed4ee8aa7034c8a934cb074a99e59
Yash Kumar Gupta <ykgupta2411@gmail.com>
2022-11-23 08:36:18 +0530

    Added program on type conversion

---
+++ classwork24.py
@@ -0,0 +1,12 @@
+# Type Converion
+# Implicit Type Conversion
+a = 10
+print(type(a))
+a = 7.86
+print(type(a))
+
+# Explicit Type Conversion
+b = int(input("Enter number: "))
+print(type(b))
+b = str(b)
+print(type(b))
