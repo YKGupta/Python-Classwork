4dcb59cc7ce694dfff9728d1d01b849b7f58c22f
Yash Kumar Gupta <ykgupta2411@gmail.com>
2022-12-04 09:17:03 +0530

    Question 1- Write a Python function to sum all the numbers in a list

---
+++ classwork33.py
@@ -0,0 +1,9 @@
+# Write a Python function to sum all the numbers in a list.
+l = []
+n = int(input("Enter range: "))
+for i in range(n):
+    l.append(int(input("Enter the element: ")))
+f = 0
+for i in l:
+    f += i
+print("Sum is:", f)
