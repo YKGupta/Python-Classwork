0fc3067a35e43106e712e3a1bb1ce489859a2727
Yash Kumar Gupta <ykgupta2411@gmail.com>
2022-12-11 13:06:28 +0530

    Question on string slicing and membership operator

---
+++ classwork30.py
@@ -0,0 +1,12 @@
+# Q - Check if substring is present in a string?
+# Q - Do slicing of string
+s = "Yash Kumar Gupta"
+if("ta" in s):
+    print("Yes")
+else:
+    print("No")
+
+print(s[5:])
+print(s[:10])
+print(s[5:10])
+print(s[:])
