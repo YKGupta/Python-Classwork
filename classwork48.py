7cc048578a6210f73236f6b95ae6896111c9309d
Yash Kumar Gupta <ykgupta2411@gmail.com>
2022-12-14 09:08:20 +0530

    Find the maximum element present in a list.

--- classwork48.py
+++ classwork48.py
@@ -2,8 +2,10 @@
 # First, you have to take 5 inputs from the user and store it in a list.
 # The, find the maximum number present in it.
 
-a, b, c, d, e = int(input("Enter 1st Number: ")), int(input("Enter 2nd Number: ")), int(input("Enter 3rd Number: ")), int(input("Enter 4th Number: ")), int(input("Enter 5th Number: "))
-l = [a, b, c, d, e]
+l = []
+for i in range(5):
+    l.append(int(input("Enter element: ")))
+
 maxx = l[0]
 for i in l:
     if (maxx < i):
