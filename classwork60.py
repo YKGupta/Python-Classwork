df0635f1c4a0469da79373616bf3ce6a22a79864
Yash Kumar Gupta <ykgupta2411@gmail.com>
2023-01-04 08:41:49 +0530

    Introduction to Exception Handling

---
+++ classwork60.py
@@ -0,0 +1,8 @@
+# Exception Handling
+fruits = ["apple", "banana"]
+try:
+    print(fruits[0])
+    print(fruits[1])
+    print(fruits[2])        # Error prone code
+except:
+    print("Something went wrong :(")
